81851317bdf5a62b1afdba952c49a6164ef85044
Kevin Dutzy <kddutz15@gmail.com>
2022-10-20 19:37:10 -0400

    added new images, changed background, linked new info, and created table

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const h2 = document.createElement("h2");
 h2.textContent = "| The worlds first home built airplane |";
 h2.style.textAlign = "center";
-h2.style.padding = "20px";
+h2.style.paddingBottom = "10px";
+h2.style.marginTop = "0px";
 document.querySelector("body").appendChild(h2);
 
