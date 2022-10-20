bfe148c33aafdef8116ee66fb4ca4030fcf3d33d
Kevin Dutzy <kddutz15@gmail.com>
2022-10-20 02:05:34 -0400

    main paragraph and some styling

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const h2 = document.createElement("h2");
-h2.textContent = "The worlds first home built airplane";
+h2.textContent = "| The worlds first home built airplane |";
 h2.style.textAlign = "center";
+h2.style.padding = "20px";
 document.querySelector("body").appendChild(h2);
 
